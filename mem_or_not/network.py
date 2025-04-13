02fa622263344055890f793b68bebdfc76f701ae
rmadeye <rafal.madaj@protonmail.com>
2025-04-13 20:21:52 +0200

    Metrics and implementation
    Adds new metrics (confusion matrix, f1, recall)
    Implements metrics into network and training
    Changes hparams file

--- mem_or_not/network.py
+++ mem_or_not/network.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+from sklearn.metrics import f1_score, precision_score, recall_score
 import torchvision.models as models
 import torchmetrics
 import torch.nn as nn
@@ -7,13 +8,15 @@ import torch
 
 
 class MemeClassifier(pl.LightningModule):
-    def __init__(self, dropout_rate=0.3):
+    def __init__(self, dropout_rate, lr):
         super().__init__()
         self.model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-
+        self.dropout_rate = float(dropout_rate)
+        self.lr = float(lr)
+        self.test_outputs = []
         in_features = self.model.fc.in_features
         self.model.fc = nn.Sequential(
-            nn.Dropout(p=dropout_rate),
+            nn.Dropout(p=self.dropout_rate),
             nn.Linear(in_features, 2),
         )
 
@@ -48,17 +51,45 @@ class MemeClassifier(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        loss = self.loss_fn(y_hat, y)  # Loss expecting y as [batch_size]
-        acc = self.accuracy(
-            y_hat, y
-        )  # Accuracy will also work with logits directly
-        probabilities = torch.softmax(y_hat, dim=1)
+        loss = self.loss_fn(y_hat, y)
+        proba = torch.softmax(y_hat, dim=1)
+        preds = torch.argmax(proba, dim=1)
+
+        acc = self.accuracy(preds, y)
+        f1 = f1_score(
+            y.detach().cpu().numpy(),
+            preds.detach().cpu().numpy(),
+            average="macro",
+        )
+        precision = precision_score(
+            y.detach().cpu().numpy(),
+            preds.detach().cpu().numpy(),
+            average="macro",
+        )
+        recall = recall_score(
+            y.detach().cpu().numpy(),
+            preds.detach().cpu().numpy(),
+            average="macro",
+        )
         self.log("test_acc", acc, prog_bar=True)
-        return {
-            "test_loss": loss,
-            "test_acc": acc,
-            "test_probabilities": probabilities,
-        }
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_f1", f1, prog_bar=True)
+        self.log("test_precision", precision, prog_bar=True)
+        self.log("test_recall", recall, prog_bar=True)
+        self.test_outputs.append(
+            {
+                "loss": loss.detach().cpu(),
+                "acc": acc.detach().cpu(),
+                "y_true": y.detach().cpu(),
+                "y_pred": preds.detach().cpu(),
+                "y_proba": proba.detach().cpu(),
+                "f1": f1,
+                "precision": precision,
+                "recall": recall,
+            }
+        )
+
+        return
 
     def predict_step(self, batch, batch_idx):
         x, _ = batch
@@ -67,4 +98,4 @@ class MemeClassifier(pl.LightningModule):
         return probabilities
 
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=1e-4)
+        return optim.Adam(self.parameters(), lr=self.lr)
